7d4b22924144e3220d2623eb49f61680406cc0dc
nyb <nyb@qq.com>
2017-10-20 14:49:59 +0800

    网络获取视频播放

---
+++ app/src/main/java/com/example/dell/video/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.dell.video;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private SurfaceView surfaceView;
+    private Button btnPause, btnPlayUrl;
+    private SeekBar skbProgress;
+    private Player player;
+    private Button btncb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        surfaceView = (SurfaceView) this.findViewById(R.id.surfaceView1);
+
+        btnPlayUrl = (Button) this.findViewById(R.id.btnPlayUrl);
+        btnPlayUrl.setOnClickListener(new ClickEvent());
+
+        btnPause = (Button) this.findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new ClickEvent());
+
+        btncb = (Button) this.findViewById(R.id.btncb);
+        btncb.setOnClickListener(new ClickEvent());
+
+        skbProgress = (SeekBar) this.findViewById(R.id.skbProgress);
+        skbProgress.setOnSeekBarChangeListener(new SeekBarChangeEvent());
+        player = new Player(surfaceView, skbProgress);
+    }
+
+    class ClickEvent implements OnClickListener {
+
+        @Override
+        public void onClick(View arg0) {
+            if (arg0 == btnPause) {
+                player.pause();
+            } else if (arg0 == btnPlayUrl) {
+                String url="http://172.17.29.27/zhangyuanayng.mp4";
+                player.playUrl(url);
+            } else if(arg0==btncb){
+                player.play();
+            }
+
+        }
+    }
+
+    class SeekBarChangeEvent implements SeekBar.OnSeekBarChangeListener {
+        int progress;
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            // 原本是(progress/seekBar.getMax())*player.mediaPlayer.getDuration()
+            this.progress = progress * player.mediaPlayer.getDuration()
+                    / seekBar.getMax();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // seekTo()的参数是相对与影片时间的数字，而不是与seekBar.getMax()相对的数字
+            player.mediaPlayer.seekTo(progress);
+        }
+    }
+}
